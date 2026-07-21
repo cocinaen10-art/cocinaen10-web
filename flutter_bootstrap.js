9d09abdd988cbde6a018ae7a58676b8dc14488f8
CocinaEn10 <cocinaen10@gmail.com>
2026-07-21 23:27:03 +0200

    Añadir formulario de comentarios

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1515778273" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "773561822" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
